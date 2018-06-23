fda910e77c31fc15712c92b1012d6a1307fa3c23
swissarmykirpan <gurpreet@gurpreetsohal.com>
2018-06-23 19:51:46 +0100

    added support for custom labels

---
+++ TestContainers.Tests/ContainerTests/ContainerTests.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Docker.DotNet.Models;
+using TestContainers.Core.Builders;
+using TestContainers.Core.Containers;
+using Xunit;
+
+namespace TestContainers.Tests.ContainerTests
+{
+    public class GenericContainerFixture : IAsyncLifetime
+    {
+        public ContainerInspectResponse ContainerInfo => _container.ContainerInspectResponse;
+        readonly Container _container;
+
+        public GenericContainerFixture() => _container = new GenericContainerBuilder<Container>()
+            .Begin()
+            .WithImage("alpine:latest")
+            .WithLabel(("your.custom", "label"))
+            .Build();
+
+        public Task InitializeAsync() => _container.Start();
+
+        public Task DisposeAsync() => _container.Stop();
+
+    }
+
+    public class GenericContainerTests : IClassFixture<GenericContainerFixture>
+    {
+        readonly ContainerInspectResponse _containerInfo;
+        public GenericContainerTests(GenericContainerFixture fixture) => _containerInfo = fixture.ContainerInfo;
+        
+        [Fact]
+        public void CustomLabelTest()
+        {
+            var label = _containerInfo.Config.Labels.Single();
+            Assert.Equal("your.custom", label.Key);
+            Assert.Equal("label", label.Value);
+        }
+    }
+
+}
