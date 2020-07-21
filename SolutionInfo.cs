f0b56b7592888a050b5c2cae58ad3fc0798469e0
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2020-07-21 18:36:15 +0200

    Remove unecessary SolutionInfo.cs and use auto-generated assembly attributes; Update build packages.

--- SolutionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyProduct("Testcontainers .NET")]
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
-[assembly: AssemblyCopyright("Copyright (c) 2018")]
